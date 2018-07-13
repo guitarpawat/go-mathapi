c24ffa7288442cfd50976ed9ca724ba3fee73acf
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-07-14 00:16:04 +0700

    Add MustPeek and MustPop to stack

--- stack/stack.go
+++ stack/stack.go
@@ -33,15 +33,25 @@ func (o *Stack) Peek() (string, error) {
 	if o.IsEmpty() {
 		return "", errors.New("error: stack is empty")
 	}
-	return o.data[o.Length()-1], nil
+	return o.MustPeek(), nil
+}
+
+// MustPeek peeks data from the stack or panic
+func (o *Stack) MustPeek() string {
+	return o.data[o.Length()-1]
 }
 
 // Pop (get and remove) the lastest data from the stack
 func (o *Stack) Pop() (string, error) {
-	temp, err := o.Peek()
-	if err != nil {
-		return "", err
+	if o.IsEmpty() {
+		return "", errors.New("error: stack is empty")
 	}
+	return o.MustPop(), nil
+}
+
+// MustPop pops data from the stack or panic
+func (o *Stack) MustPop() string {
+	temp := o.MustPeek()
 	o.data = o.data[:o.Length()-1]
-	return temp, nil
+	return temp
 }

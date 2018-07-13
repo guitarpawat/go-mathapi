3dc97610d6ec24e25fa1beeafbd2dbe33b2937db
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-07-13 19:30:52 +0700

    Add stack comment

--- stack/stack.go
+++ stack/stack.go
@@ -1,27 +1,34 @@
+// Package stack provides convenient using in this project, without casting the interface{}
 package stack
 
 import "errors"
 
+// Stack is the stack of string for using in this project
 type Stack struct {
 	data []string
 }
 
+// NewStack return the pointer to new stack
 func NewStack() *Stack {
 	return new(Stack)
 }
 
+// IsEmpty returns true only when length of data in the stack is zero
 func (o *Stack) IsEmpty() bool {
 	return o.Length() == 0
 }
 
+// Length of this stack
 func (o *Stack) Length() int {
 	return len(o.data)
 }
 
+// Push string to the stack
 func (o *Stack) Push(s string) {
 	o.data = append(o.data, s)
 }
 
+// Peek (get) the lastest data from the stack
 func (o *Stack) Peek() (string, error) {
 	if o.IsEmpty() {
 		return "", errors.New("error: stack is empty")
@@ -29,6 +36,7 @@ func (o *Stack) Peek() (string, error) {
 	return o.data[o.Length()-1], nil
 }
 
+// Pop (get and remove) the lastest data from the stack
 func (o *Stack) Pop() (string, error) {
 	temp, err := o.Peek()
 	if err != nil {

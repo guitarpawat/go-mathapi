9c2bb4578b27e52f20fa5d1900c3d7025f9fd783
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-07-14 23:48:30 +0700

    Add host and port flag to main

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/guitarpawat/go-mathapi/api"
 )
 
+var host = flag.String("host", "localhost", "Specify running host of API")
+var port = flag.Int("port", 8000, "Specify running host of API")
+
 func main() {
+	flag.Parse()
 	http.HandleFunc("/", api.APIHandler)
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", *host, *port), nil)
 	log.Fatalln(err)
 }

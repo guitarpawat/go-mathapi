f143edb312d15635edc613ca2f1ab88d14bcc5c9
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-07-15 16:16:52 +0700

    Add redirection case

--- api/api.go
+++ api/api.go
@@ -10,7 +10,18 @@ import (
 
 // APIHandler handles the http requst
 func APIHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		IndexHandler(w, r)
+		return
+	}
+
+	if string(r.URL.Path[len(r.URL.Path)-1]) == "/" {
+		http.Redirect(w, r, string(r.URL.Path[:len(r.URL.Path)-1]), http.StatusSeeOther)
+		return
+	}
+
 	token := strings.Split(r.URL.Path, "/")
+
 	result, err := evaluate.Evaluate(token[1:])
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
--- api/api_test.go
+++ api/api_test.go
@@ -26,7 +26,7 @@ func Test_APIHandler_OK(t *testing.T) {
 
 	expected := "2.0000"
 	if strings.TrimSpace(string(actual)) != expected {
-		t.Errorf("expected result:|%s| but get:|%s|", expected, actual)
+		t.Errorf("expected result: %s but get: %s", expected, actual)
 	}
 }
 
@@ -38,5 +38,40 @@ func Test_APIHandler_Error(t *testing.T) {
 
 	if resp.StatusCode != http.StatusInternalServerError {
 		t.Errorf("expected status code: %d but get: %d", http.StatusInternalServerError, resp.StatusCode)
+	}
+}
+
+func Test_APIHandler_ExtraSlash_SeeOther(t *testing.T) {
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/5/mod/3/", nil)
+	APIHandler(res, req)
+	resp := res.Result()
+
+	if resp.StatusCode != http.StatusSeeOther {
+		t.Errorf("expected status code: %d but get: %d", http.StatusOK, resp.StatusCode)
+		return
+	}
+}
+
+func Test_APIHandler_Slash_ToIndexHandler(t *testing.T) {
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	APIHandler(res, req)
+	resp := res.Result()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("expected status code: %d but get: %d", http.StatusOK, resp.StatusCode)
+		return
+	}
+
+	actual, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("unexpected error when reading body: %s", err)
+		return
+	}
+
+	expected := "<!DOCTYPE html>"
+	if !strings.ContainsAny(string(actual), expected) {
+		t.Errorf("result does not contains: %s only get:\n%s", expected, string(actual))
 	}
 }

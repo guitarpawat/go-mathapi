81328f873ef64f07104171032233e4f13932d942
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-07-18 22:58:12 +0700

    Add mem handler

--- api/api.go
+++ api/api.go
@@ -25,6 +25,11 @@ func APIHandler(w http.ResponseWriter, r *http.Request) {
 
 	token = preprocess.ConstantToNumber(token)
 
+	if token[1] == "mem" {
+		MemHandler(w, token[2:]...)
+		return
+	}
+
 	result, err := evaluate.Evaluate(token[1:])
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -34,4 +39,25 @@ func APIHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintln(w, result)
+}
+
+func MemHandler(w http.ResponseWriter, infix ...string) {
+	errMsg := "expected format: /mem/name/number or /mem/name/infix"
+	if len(infix) < 2 {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, errMsg)
+		return
+	}
+
+	err := preprocess.AddToMem(infix[0], infix[1:]...)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
+
+	value, _ := preprocess.GetFromMem(infix[0])
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Remember %s as %s", infix[0], value)
 }

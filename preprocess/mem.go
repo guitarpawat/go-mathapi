9f586ebbba7915bec7a6b11c159d994ae01b5039
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2018-07-18 22:24:05 +0700

    Add essential funcs to mem

--- preprocess/mem.go
+++ preprocess/mem.go
@@ -1,4 +1,37 @@
 package preprocess
 
-// MemMap remember custom constants
-var MemMap = map[string]string{}
+import (
+	"fmt"
+
+	"github.com/guitarpawat/go-mathapi/evaluate"
+)
+
+var memMap = map[string]string{}
+
+// AddToMem stores memory(value) to the key of map
+func AddToMem(key string, infix ...string) error {
+	result, err := evaluate.Evaluate(infix)
+	if err != nil {
+		return fmt.Errorf("cannot add to memory: %s", err)
+	}
+
+	memMap[key] = result
+	return nil
+}
+
+// GetFromMem gets value of the specified key in the map
+func GetFromMem(key string) (string, bool) {
+	value, ok := memMap[key]
+	return value, ok
+}
+
+// RemoveFromMem removes value of the specified key from the map
+func RemoveFromMem(key string) (string, bool) {
+	value, ok := GetFromMem(key)
+	if !ok {
+		return "", false
+	}
+
+	delete(memMap, key)
+	return value, true
+}

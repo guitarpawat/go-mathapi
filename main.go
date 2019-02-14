04eb32099312f91dadfcbf99491b7dcdb0c03457
Pawat Nakpiphatkul <guitar.pawat@gmail.com>
2019-02-15 00:11:51 +0700

    Listen to heroku port

--- main.go
+++ main.go
@@ -5,16 +5,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/guitarpawat/go-mathapi/api"
 )
 
 var host = flag.String("host", "localhost", "Specify running host of API")
-var port = flag.Int("port", 8000, "Specify running host of API")
+var port = flag.Int("port", 0, "Specify running host of API")
 
 func main() {
 	flag.Parse()
+	var err error
+	if *port == 0 {
+		*port, err = strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			panic(err)
+		}
+	}
 	http.HandleFunc("/", api.APIHandler)
-	err := http.ListenAndServe(fmt.Sprintf("%s:%d", *host, *port), nil)
+	err = http.ListenAndServe(fmt.Sprintf("%s:%d", *host, *port), nil)
 	log.Fatalln(err)
 }
